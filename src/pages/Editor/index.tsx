ea5f97833897813e977c8421dfba149f51607dff
jalalbmnf <calalmnf@gmail.com>
2024-09-30 00:00:47 +0400

    Commented unused import

--- src/pages/Editor/index.tsx
+++ src/pages/Editor/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import PreviewColumn from "@/components/PreviewColumn";
+// import PreviewColumn from "@/components/PreviewColumn";
 import MarkdownEditor from "@/components/MarkdownEditor";
 
 import "./style.css";
